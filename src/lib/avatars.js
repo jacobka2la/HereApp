85082f4a4d7ca626917dfa68ad49cd51c70317d5
jacobka2la <162231046+jacobka2la@users.noreply.github.com>
2026-08-15 17:02:18 -0400

    Use final generated avatar artwork

--- src/lib/avatars.js
+++ src/lib/avatars.js
@@ -1,8 +1,8 @@
 export const avatars = [
-  { id: 'avatar-1', skin: '#f1b07f', hair: '#7a3b16', drink: '#e53935', drinkType: 'cup' },
-  { id: 'avatar-2', skin: '#7a4327', hair: '#17110f', drink: '#a76324', drinkType: 'rocks' },
-  { id: 'avatar-3', skin: '#efb17f', hair: '#e0ad61', drink: '#ead68c', drinkType: 'wine' },
-  { id: 'avatar-4', skin: '#8b4b2d', hair: '#161012', drink: '#f36f8d', drinkType: 'straw' },
+  { id: 'avatar-1', image: '/avatars/avatar-1.jpg', alt: 'Avatar option 1' },
+  { id: 'avatar-2', image: '/avatars/avatar-2.jpg', alt: 'Avatar option 2' },
+  { id: 'avatar-3', image: '/avatars/avatar-3.jpg', alt: 'Avatar option 3' },
+  { id: 'avatar-4', image: '/avatars/avatar-4.jpg', alt: 'Avatar option 4' },
 ];
 
 export function getAvatarById(id) {
