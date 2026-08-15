99b0d2303478475e4827ec0ef3d2a71090529e86
jacobka2la <162231046+jacobka2la@users.noreply.github.com>
2026-08-15 09:53:24 -0400

    Capitalize search label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 export default function SearchBar({ value, onChange }) {
   return (
     <label className="search-wrap">
-      <span>Search bars</span>
+      <span>Search Bars</span>
       <input
         value={value}
         onChange={(event) => onChange(event.target.value)}
