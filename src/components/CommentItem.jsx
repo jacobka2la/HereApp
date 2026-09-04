b16394fa098e82b1c14431248c6a8e3debaf9bc0
jacobka2la <162231046+jacobka2la@users.noreply.github.com>
2026-09-03 20:20:04 -0400

    Show selected avatars on comments

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
 import { reactionOptions } from '../lib/bars';
 import { formatRelativeTime } from '../lib/day';
+import { getPublicProfilesByUids } from '../lib/firebaseHelpers';
 import UserAvatar from './UserAvatar';
 
 export default function CommentItem({ comment, reactionCounts, activeReaction, onReact }) {
+  const [profileAvatarId, setProfileAvatarId] = useState('');
+
+  useEffect(() => {
+    let cancelled = false;
+
+    if (!comment?.uid || comment?.avatarId) {
+      setProfileAvatarId('');
+      return () => { cancelled = true; };
+    }
+
+    getPublicProfilesByUids([comment.uid])
+      .then((profiles) => {
+        if (!cancelled) setProfileAvatarId(profiles?.[comment.uid]?.avatarId || '');
+      })
+      .catch(() => {
+        if (!cancelled) setProfileAvatarId('');
+      });
+
+    return () => { cancelled = true; };
+  }, [comment?.uid, comment?.avatarId]);
+
+  const avatarId = comment.avatarId || profileAvatarId;
+
   return (
     <article className="comment-card">
       <div className="comment-topline">
         <div className="comment-author">
-          <UserAvatar username={comment.username} avatarId={comment.avatarId} size="sm" />
+          <UserAvatar username={comment.username} avatarId={avatarId} size="sm" />
           <strong className="comment-username">@{comment.username}</strong>
         </div>
         <span className="comment-time">{formatRelativeTime(comment.createdAtMillis)}</span>
